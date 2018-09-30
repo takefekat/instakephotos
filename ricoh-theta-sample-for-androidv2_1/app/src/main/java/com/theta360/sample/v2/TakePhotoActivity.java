6a8993fdd93730da5ac5049fcd29642c8a7f76dc
junkawaguchi <junki.kawaguchi@gmail.com>
2018-09-30 21:28:51 +0900

    tensorflow用

--- ricoh-theta-sample-for-androidv2_1/app/src/main/java/com/theta360/sample/v2/TakePhotoActivity.java
+++ ricoh-theta-sample-for-androidv2_1/app/src/main/java/com/theta360/sample/v2/TakePhotoActivity.java
@@ -6,6 +6,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
 import android.net.ConnectivityManager;
 import android.net.Network;
 import android.net.NetworkCapabilities;
@@ -34,7 +35,6 @@ import com.theta360.sample.v2.network.HttpConnector;
 import com.theta360.sample.v2.network.HttpDownloadListener;
 import com.theta360.sample.v2.network.HttpEventListener;
 import com.theta360.sample.v2.network.ImageData;
-//import com.theta360.sample.v2.view.LogView;
 import com.theta360.sample.v2.view.MJpegInputStream;
 import com.theta360.sample.v2.view.MJpegView;
 
@@ -54,7 +54,7 @@ import java.io.InputStream;
 import java.io.PrintWriter;
 import java.math.BigDecimal;
 import java.util.ArrayList;
-
+import java.util.Collections;
 
 /**
  * Activity that displays the photo list
@@ -613,13 +613,31 @@ public class TakePhotoActivity extends Activity {
             image2d_bitmap_a = cutter.cut(image360_bitmap,pitch_roll_yaw,1.0,1.0,2 * Math.PI / 10,Math.PI /2/4);
             Log.d("debug","Complete cut image");
 
+            //TODO 上位何個を出力するか定数として一か所で定義するか検討
+            int output_num = 5;
+
+            //TODO モデル読み込みは事前の実施が可能(位置変更可)
+            //モデル読み込み
+            MyTensorFlow myTensorFlow = new MyTensorFlow();
+            myTensorFlow.modelCreate();
+
+            //TODO ビットマップの数と合わせる
+            //切り取った画像を認識してinstabaeを抽出(resultsに代入)
+            for(int i = 0; i < 50; i++){
+               myTensorFlow.startRecognition(image2d_bitmap_a[i], Integer.toString(i));
+            }
+
+            //resultsを認識率の高い順にソート
+            Collections.sort(myTensorFlow.results, new Classifier.ScoreCmp());
+
+            //識別率の高い画像の番号を保存
             ArrayList<Integer> output_photos = new ArrayList();
-            output_photos.add(0);
-            output_photos.add(5);
-            output_photos.add(10);
-            output_photos.add(15);
-            output_photos.add(20);
-            output_photos.add(24);
+            for(int i = 0; i < output_num; i++){
+                output_photos.add(Integer.parseInt(myTensorFlow.results.get(i).getImageId()));
+            }
+
+            //モデルクローズ
+            myTensorFlow.endRecognition();
 
             Log.d("debug","Complete cut image");
 
@@ -855,5 +873,160 @@ public class TakePhotoActivity extends Activity {
             }
         }
     }
+    public class MyTensorFlow {
+        // These are the settings for the original v1 Inception model. If you want to
+        // use a model that's been produced from the TensorFlow for Poets codelab,
+        // you'll need to set IMAGE_SIZE = 299, IMAGE_MEAN = 128, IMAGE_STD = 128,
+        // INPUT_NAME = "Mul", and OUTPUT_NAME = "final_result".
+        // You'll also need to update the MODEL_FILE and LABEL_FILE paths to point to
+        // the ones you produced.
+        //
+        // To use v3 Inception model, strip the DecodeJpeg Op from your retrained
+        // model first:
+        //
+        // python strip_unused.py \
+        // --input_graph=<retrained-pb-file> \
+        // --output_graph=<your-stripped-pb-file> \
+        // --input_node_names="Mul" \
+        // --output_node_names="final_result" \
+        // --input_binary=true
+        private final int INPUT_SIZE;
+        private final int IMAGE_MEAN;
+        private final float IMAGE_STD;
+        private final String INPUT_NAME;
+        private final String OUTPUT_NAME;
+        //private static final int INPUT_SIZE = 224;
+        //private static final int IMAGE_MEAN = 117;
+        //private static final float IMAGE_STD = 1;
+        //private static final String INPUT_NAME = "input";
+        //private static final String OUTPUT_NAME = "output";
 
+        private final String MODEL_FILE;
+        private final String LABEL_FILE;
+
+        private Bitmap croppedBitmap;
+        private Matrix imageToCropTransform;
+
+        private int inputHeight;
+        private int inputWidth;
+
+        private Classifier classifier;
+
+        ArrayList<Classifier.Recognition> results = new ArrayList<Classifier.Recognition>();
+
+        MyTensorFlow() {
+            INPUT_SIZE = 299;
+            IMAGE_MEAN = 128;
+            IMAGE_STD = 128;
+            //TODO strip_unused.py実行時には--input_node_names, INPUT_NAME="Mul"にするべきかも
+            INPUT_NAME = "Placeholder";
+//            INPUT_NAME = "Mul";
+            OUTPUT_NAME = "final_result";
+            //TODO MODEL_FILEはどちらかを選択
+            MODEL_FILE = "file:///android_asset/instabae_graph_android_unused.pb";
+//            MODEL_FILE = "file:///android_asset/instabae_graph_android.pb";
+            LABEL_FILE = "file:///android_asset/instabae_labels.txt";
+
+            croppedBitmap = null;
+        }
+
+        public void modelCreate() {
+            classifier = TensorFlowImageClassifier.create(
+                    getAssets(),
+                    MODEL_FILE,
+                    LABEL_FILE,
+                    INPUT_SIZE,
+                    IMAGE_MEAN,
+                    IMAGE_STD,
+                    INPUT_NAME,
+                    OUTPUT_NAME
+            );
+        }
+
+        public void endRecognition() {
+                classifier.close();
+        }
+
+        private void startRecognition(Bitmap _inputImage, String _image_num) {
+
+            inputWidth = _inputImage.getWidth();
+            inputHeight = _inputImage.getHeight();
+            croppedBitmap = Bitmap.createBitmap(INPUT_SIZE, INPUT_SIZE, Bitmap.Config.ARGB_8888);
+
+            imageToCropTransform = getTransformationMatrix(
+                    inputWidth, inputHeight,
+                    INPUT_SIZE, INPUT_SIZE,
+                    0, true);//sensorOrientation, MAINTAIN_ASPECT
+
+            croppedBitmap = Bitmap.createBitmap(_inputImage, 0, 0, _inputImage.getWidth(), _inputImage.getHeight(), imageToCropTransform, true);
+            results.add(classifier.recognizeImage(croppedBitmap, _image_num));
+        }
+
+        /**
+         * Returns a transformation matrix from one reference frame into another.
+         * Handles cropping (if maintaining aspect ratio is desired) and rotation.
+         *
+         * @param srcWidth Width of source frame.
+         * @param srcHeight Height of source frame.
+         * @param dstWidth Width of destination frame.
+         * @param dstHeight Height of destination frame.
+         * @param applyRotation Amount of rotation to apply from one frame to another.
+         *  Must be a multiple of 90.
+         * @param maintainAspectRatio If true, will ensure that scaling in x and y remains constant,
+         * cropping the image if necessary.
+         * @return The transformation fulfilling the desired requirements.
+         */
+        public Matrix getTransformationMatrix(
+                final int srcWidth,
+                final int srcHeight,
+                final int dstWidth,
+                final int dstHeight,
+                final int applyRotation,
+                final boolean maintainAspectRatio) {
+            final Matrix matrix = new Matrix();
+
+            if (applyRotation != 0) {
+                if (applyRotation % 90 != 0) {
+                    //LOGGER.w("Rotation of %d % 90 != 0", applyRotation);
+                }
+
+                // Translate so center of image is at origin.
+                matrix.postTranslate(-srcWidth / 2.0f, -srcHeight / 2.0f);
+
+                // Rotate around origin.
+                matrix.postRotate(applyRotation);
+            }
+
+            // Account for the already applied rotation, if any, and then determine how
+            // much scaling is needed for each axis.
+            final boolean transpose = (Math.abs(applyRotation) + 90) % 180 == 0;
+
+            final int inWidth = transpose ? srcHeight : srcWidth;
+            final int inHeight = transpose ? srcWidth : srcHeight;
+
+            // Apply scaling if necessary.
+            if (inWidth != dstWidth || inHeight != dstHeight) {
+                final float scaleFactorX = dstWidth / (float) inWidth;
+                final float scaleFactorY = dstHeight / (float) inHeight;
+
+                if (maintainAspectRatio) {
+                    // Scale by minimum factor so that dst is filled completely while
+                    // maintaining the aspect ratio. Some image may fall off the edge.
+                    final float scaleFactor = Math.max(scaleFactorX, scaleFactorY);
+                    //final float scaleFactor = Math.min(scaleFactorX, scaleFactorY);
+                    matrix.postScale(scaleFactor, scaleFactor);
+                } else {
+                    // Scale exactly to fill dst from src.
+                    matrix.postScale(scaleFactorX, scaleFactorY);
+                }
+            }
+
+            if (applyRotation != 0) {
+                // Translate back from origin centered reference to destination frame.
+                matrix.postTranslate(dstWidth / 2.0f, dstHeight / 2.0f);
+            }
+
+            return matrix;
+        }
+    }
 }
